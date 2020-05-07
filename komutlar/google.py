e15b4ebbc074e26dc144378f200f481853107578
izci-py <64651153+izci-py@users.noreply.github.com>
2020-05-07 14:47:12 +0300

    Update google.py

--- komutlar/google.py
+++ komutlar/google.py
@@ -10,19 +10,20 @@ def google_search(client, message):
     if len(text.split()) == 1:
         message.edit("Lütfen araştırmak istediğiniz kelimeyi giriniz")
         return
-    query = " ".join(text.split()[1:])
-    msg = "Araştırılan Kelime : {}\n\n".format(query)
-    res = GoogleSearchClient()
-    results = res.search(query).to_json()
-    if results:
-        i = 1
-        for result in ast.literal_eval(results):
-            msg += f"🔍 [{result['title']}]({result['url']})\n\n"
-            i += 1
-            if i == 10:
-                break
+    else:
+        query = " ".join(text.split()[1:])
+        msg = "Araştırılan Kelime : {}\n\n".format(query)
+        res = GoogleSearchClient()
+        results = res.search(query).to_json()
+        if results:
+            i = 1
+            for result in ast.literal_eval(results):
+                msg += f"🔍 [{result['title']}]({result['url']})\n\n"
+                i += 1
+                if i == 10:
+                    break
 
-        try:
-            bekle.edit(msg, disable_web_page_preview=True, parse_mode="Markdown")
-        except Exception as e:
-            print(e)
+            try:
+                bekle.edit(msg, disable_web_page_preview=True, parse_mode="Markdown")
+            except Exception as e:
+                print(e)
